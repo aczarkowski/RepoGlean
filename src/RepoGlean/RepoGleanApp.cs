27c487aed4f63bbab060acada924744f5802abbf
Andrzej Czarkowski <aczarkowski@gmail.com>
2026-08-10 21:21:51 +0100

    build: prepare RepoGlean v2.3.0

--- src/RepoGlean/RepoGleanApp.cs
+++ src/RepoGlean/RepoGleanApp.cs
@@ -898,7 +898,7 @@ public static class RepoGleanApp
         var assembly = typeof(RepoGleanApp).Assembly;
         var informational = assembly.GetCustomAttribute<AssemblyInformationalVersionAttribute>()?.InformationalVersion;
         if (!string.IsNullOrWhiteSpace(informational)) return informational.Split('+')[0];
-        return assembly.GetName().Version?.ToString(3) ?? "2.2.0";
+        return assembly.GetName().Version?.ToString(3) ?? "2.3.0";
     }
 
     private static void WriteHelp(TextWriter output)
